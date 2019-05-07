474c4ad7cbb354419cb747d6f2afe213a9b510dc
Toory <giu.garone@gmail.com>
2019-05-07 11:08:40 +0200

    init commit

---
+++ mquoteGUI.py
@@ -0,0 +1,147 @@
+import trade
+import sys
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QWidget, QPushButton, QHBoxLayout,QLabel, QApplication, QVBoxLayout, QCalendarWidget, QTabWidget
+from PyQt5.QtCore import Qt, QDate
+from PyQt5.QtGui import QIcon, QPixmap
+
+class TradeMainWindow(QMainWindow):
+
+	def __init__(self, parent=None):
+		super(TradeMainWindow, self).__init__(parent)
+		self.form_widget = TradeWidget(self)
+		self.initUI()
+		self.setCentralWidget(self.form_widget)
+
+	def initUI(self):
+		self.title = 'mQuote'
+		self.setWindowTitle(self.title)
+		self.setGeometry(100,100,400,500)
+		#Icon from Freepik
+		self.setWindowIcon(QIcon(QPixmap('stock.png')))
+		self.show()
+
+class TradeWidget(QWidget):
+
+	def __init__(self, parent):
+		super(TradeWidget,self).__init__(parent)
+		self.layout = QVBoxLayout(self)
+		self.tabs = QTabWidget()
+		self.tab1 = QWidget()
+		self.tab2 = QWidget()
+		self.tabs.addTab(self.tab1,"Daily Stocks")
+		self.tabs.addTab(self.tab2,"Choose a Date")
+
+		# Add tab1 and tab2 UI
+		self.tab1UI()
+		self.tab2UI()
+
+		# Add tabs to widget
+		self.layout.addWidget(self.tabs)
+		self.setLayout(self.layout)
+		self.show()
+
+	def tab1UI(self):
+		self.button1 = QPushButton('Get Daily Stocks!')
+		self.text = QTextEdit()
+
+		self.tab1.v_layout = QVBoxLayout()
+		self.tab1.h_layout = QHBoxLayout()
+
+		self.tab1.h_layout.addWidget(self.button1)
+
+		self.tab1.v_layout.addWidget(self.text)
+		self.tab1.v_layout.addLayout(self.tab1.h_layout)
+
+		self.tab1.setLayout(self.tab1.v_layout)
+
+		self.button1.clicked.connect(self.btn_clicked)
+
+	def tab2UI(self):
+		#super(MyTableWidget, self).__init__()
+		self.button3 = QPushButton('Select Start Date')
+		self.button4 = QPushButton('Select End Date')
+		self.button5 = QPushButton('Get Stocks!')
+		self.text2 = QTextEdit()
+		self.label1 = QLabel('Start Date')
+		self.label2 = QLabel('End Date')
+		self.label1.setAlignment(Qt.AlignCenter)
+		self.label2.setAlignment(Qt.AlignCenter)
+
+		self.tab2.v_layout = QVBoxLayout()
+		self.tab2.h_layout = QHBoxLayout()
+		self.tab2.h2_layout = QHBoxLayout()
+		self.tab2.h3_layout = QHBoxLayout()
+
+		self.tab2.h_layout.addWidget(self.label1)
+		self.tab2.h_layout.addWidget(self.label2)
+		self.tab2.v_layout.addLayout(self.tab2.h_layout)
+
+		self.tab2.h2_layout.addWidget(self.button3)
+		self.tab2.h2_layout.addWidget(self.button4)
+		self.tab2.v_layout.addLayout(self.tab2.h2_layout)
+
+
+		self.tab2.v_layout.addWidget(self.text2)
+		self.tab2.h3_layout.addWidget(self.button5)
+		self.tab2.v_layout.addLayout(self.tab2.h3_layout)
+
+		self.tab2.setLayout(self.tab2.v_layout)
+
+		self.button3.clicked.connect(self.Calendar)
+		self.button4.clicked.connect(self.Calendar)
+		self.button5.clicked.connect(self.btn_clicked)
+
+		self.label1.installEventFilter(self)
+		self.label2.installEventFilter(self)
+
+	def btn_clicked(self):
+		self.text.clear()
+		self.text2.clear()
+		sender = self.sender()
+
+		if sender.text() == 'Get Daily Stocks!':
+			self.button1.
+			trade.Trade(1,self.text)
+			self.text.append('[+] Done!')
+		elif sender.text() == 'Get Stocks!':
+			StartDate = self.label1.text()
+			EndDate = self.label2.text()
+			trade.Trade(2,self.text2,StartDate,EndDate)
+			self.text2.append('[+] Done!')
+		else:
+			pass
+
+	def Calendar(self):
+		sender = self.sender()
+		self.cal = QCalendarWidget(self)
+		self.cal.setGridVisible(True)
+		if sender.text() == 'Select Start Date':
+			self.cal.clicked[QDate].connect(self.startDate)
+		elif sender.text() == 'Select End Date':
+			self.cal.clicked[QDate].connect(self.endDate)
+		# create a new window that contains the calendar
+		self.calendarWindow = QWidget()
+		hbox = QHBoxLayout()
+		hbox.addWidget(self.cal)
+		self.calendarWindow.setLayout(hbox)
+		self.calendarWindow.setGeometry(250, 250, 250, 250)
+		self.calendarWindow.setWindowTitle('Calendar')
+		# open this new window
+		self.cal.clicked.connect(self.closeCal)
+		self.calendarWindow.show()
+
+	def closeCal(self):
+		self.calendarWindow.hide()
+
+	def startDate(self,date):
+		self.label1.setText(date.toString('dd/MM/yyyy'))
+		return
+
+	def endDate(self,date):
+		self.label2.setText(date.toString('dd/MM/yyyy'))
+		return
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	trade_ = TradeMainWindow()
+	sys.exit(app.exec_())
---
+++ trade.py
@@ -0,0 +1,142 @@
+'''
+Credit for the Cookie Crumb fuctions goes to bradlucas
+https://github.com/bradlucas/get-yahoo-quotes-python/blob/master/get-yahoo-quotes.py
+'''
+import re
+import sys
+import time
+import datetime
+import requests
+import argparse, textwrap
+from PyQt5.QtWidgets import QTextEdit, QApplication
+from PyQt5.QtCore import Qt
+
+class Trade():
+	
+	def __init__(self,choose,text=None,tempStart='',tempEnd=''):
+		with open("stocks.txt","r+") as f:
+			stocks = f.read().splitlines()  #splitlines to get line by line output from file
+		f.close()
+		counter = 0
+
+		if choose == 1:
+			start_date,end_date = self.daily()
+		elif choose == 2:
+			start_date,end_date = self.fromDate(tempStart,tempEnd)
+		else:
+			print('Wrong input, exiting...')
+			exit(-1)
+		#get the cookie and crumb for one stock, then uses the same cookie and crumb for all iterations
+		cookie, crumb = self.get_cookie_crumb('ENI.MI')
+		for stock in stocks:
+			self.show(stock,text)
+			self.download_quotes(stock,counter,cookie,crumb,start_date,end_date)
+			counter += 1
+
+	def daily(self):
+		start_date = self.get_now_epoch()
+		tempEnd = str(datetime.date.today() + datetime.timedelta(days=1)) #get date format YYYY-MM-DD
+		year,month,day = tempEnd.split('-')
+		end_date = int(datetime.datetime(int(year),int(month),int(day)).timestamp())
+		#print(time.strftime('%Y-%m-%d', time.localtime(start_date)), start_date)
+		#print(time.strftime('%Y-%m-%d', time.localtime(end_date)), end_date)
+		return start_date,end_date
+
+	def fromDate(self,tempStart,tempEnd):
+		print(tempStart,tempEnd)
+		day,month,year = tempStart.split('/')
+		start_date = int(datetime.datetime(int(year),int(month),int(day)).timestamp())
+		day,month,year = tempEnd.split('/')
+		end_date = int(datetime.datetime(int(year),int(month),int(day)).timestamp())
+		return start_date,end_date
+
+	def split_crumb_store(self,v):
+		return v.split(':')[2].strip('"')
+
+	def find_crumb_store(self,lines):
+		# Looking for
+		# ,"CrumbStore":{"crumb":"9q.A4D1c.b9
+		for l in lines:
+			if re.findall(r'CrumbStore', l):
+				return l
+		print("Did not find CrumbStore")
+
+	def get_cookie_value(self,r):
+		return {'B': r.cookies['B']}
+
+	def get_page_data(self,symbol):
+		url = "https://finance.yahoo.com/quote/%s/?p=%s" % (symbol, symbol)
+		r = requests.get(url)
+		cookie = self.get_cookie_value(r)
+		# Code to replace possible \u002F value
+		# ,"CrumbStore":{"crumb":"FWP\u002F5EFll3U"
+		# FWP\u002F5EFll3U
+		lines = r.content.decode('unicode-escape').strip(). replace('}', '\n')
+		return cookie, lines.split('\n')
+
+	def get_cookie_crumb(self,symbol):
+		cookie, lines = self.get_page_data(symbol)
+		crumb = self.split_crumb_store(self.find_crumb_store(lines))
+		return cookie, crumb
+
+	def get_data(self,symbol, start_date, end_date, cookie, crumb, counter):
+		date = self.getformattedtoday()
+		filename = '%s.txt' % (date)
+		if (counter == 0):
+			f = open(filename, 'a')
+			firstline = '<ticker>,<name>,<date>,<per>,<open>,<high>,<low>,<close>,<vol>,<o/i>\n'
+			f.write(firstline)
+			f.close()
+		url = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=history&crumb=%s" % (symbol, start_date, end_date, crumb)
+		response = requests.get(url, cookies=cookie)
+		with open (filename, 'a') as f:
+			for line in response.iter_lines():
+				if (line[0] != 68):
+					#f.write(line)
+					#if response line has all 7 pieces of information
+					if len(line.split(str.encode(','))) == 7:
+						Date,Open,High,Low,Close,Adj_Close,Volume = line.split(str.encode(','))
+						#checks that the result of Open is not null (to avoid parsing empty data)
+						if Open.decode("utf-8") != 'null':
+							year,month,day = Date.decode("utf-8").split("-")
+							#writes stock data with the standard metastock import
+							lineToWrite = symbol  + ',????,' + month + '/' + day + '/' + year  + ',D,' + Open.decode("utf-8")  + ',' + High.decode("utf-8")  + ',' + Low.decode("utf-8")  + ',' + Close.decode("utf-8")  + ',' + Volume.decode("utf-8")  + ',0\n'
+							f.write(lineToWrite)
+							print(lineToWrite)
+		f.close()
+
+	def get_now_epoch(self):
+		# @see https://www.linuxquestions.org/questions/programming-9/python-datetime-to-epoch-4175520007/#post5244109
+		return int(time.time())
+
+	def download_quotes(self,symbol,counter, cookie, crumb,start_date,end_date):
+		self.get_data(symbol, start_date, end_date, cookie, crumb, counter)
+
+	def getformattedtoday(self):
+		return datetime.datetime.today().strftime('%Y%m%d')
+
+	def show(self,symbol,text):
+		print("[+] Downloading %s !" % symbol)
+		#string = f"[+] Downloading {symbol} from {time.strftime('%Y/%m/%d', time.localtime(start_date))} to {time.strftime('%Y/%m/%d', time.localtime(end_date))}!"
+		if text != None:
+			string = f"[+] Downloading {symbol}!"
+			text.append(string)
+			QApplication.processEvents()
+		return
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('-d', '--daily',help='Get daily stocks', action='store_true')
+	group.add_argument('-p', '--period',action='store', nargs=2,metavar=('startDate', 'endDate'),help=textwrap.dedent('''\
+	Get stocks from a specified period
+	Usage: python %(prog)s -p dd/mm/yyyy dd/mm/yyyy'''))
+	args = parser.parse_args()
+
+	if args.daily:
+		Trade(1)
+	elif args.period:
+		startDate,endDate = args.period
+		Trade(2,tempStart=startDate,tempEnd=endDate)
+	else:
+		parser.print_help()
